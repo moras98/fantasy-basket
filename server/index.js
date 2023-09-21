c114556d51c4820b4480faf13e9bd0372f76bd78
agustin <a.morascastro@gmail.com>
2023-09-21 11:22:12 -0300

    direction change

--- server/index.js
+++ server/index.js
@@ -11,10 +11,10 @@ async function startServer() {
   // Init application loaders
   loaders(app);
 
-  app.use(express.static(path.resolve(__dirname, '../client/build')));
+  app.use(express.static(path.resolve(__dirname, 'client/build')));
 
   app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
   
   // Start server
