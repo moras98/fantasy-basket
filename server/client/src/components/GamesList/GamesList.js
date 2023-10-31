e10dd8976b3fe018bdfabaa19daeaca47d310a10
Agustin Moras <a.morascastro@gmail.com>
2023-10-31 19:40:15 -0300

    seleccionador jornada

--- server/client/src/components/GamesList/GamesList.js
+++ server/client/src/components/GamesList/GamesList.js
@@ -10,6 +10,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { loadGames } from '../../store/game/Game.actions';
 import { loadTeams } from '../../store/team/Team.actions';
 import { useNavigate } from 'react-router-dom';
+import './GamesList.css';
+import Button from '../Button/Button';
 
 
 export default function GamesList(){
@@ -17,6 +19,8 @@ export default function GamesList(){
     const games = useSelector(state => state.games);
     const teams = useSelector(state => state.teams);
     const navigate = useNavigate();
+
+    const [currentMatchweek, setCurrentMatchweek] = React.useState(1);
 
     React.useEffect(()=>{
         async function load(){
@@ -26,33 +30,62 @@ export default function GamesList(){
         load();
     }, [dispatch]);
 
+    const gamesArray = Object.keys(games).map(key => games[key]);
+    const filteredGames = gamesArray.filter(game => game.matchweek_id == currentMatchweek);
+
+    console.log(filteredGames);
+
+
     return(
-        <TableContainer component={Paper}>
-            <Table aria-label='games table'>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Jornada</TableCell>
-                        <TableCell align='right'>Fecha</TableCell>
-                        <TableCell align='right'>Resultado</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {games && Object.keys(games).length > 0 && Object.keys(games).map(key=>{
-                        const game = games[key];
-                        return(
-                            <TableRow 
-                            key={game.id}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 }, '&:hover': { backgroundColor: '#f2f2f2' } }}
-                            onClick={()=>{navigate(`/games/${game.id}`)}}
-                            >
-                                <TableCell>{game.matchweek_id}</TableCell>
-                                <TableCell align='right'>{game.date.split('T')[0]}</TableCell>
-                                <TableCell align='right'>{teams[game.team1_id]?.name} {game.team1_score} - {game.team2_score} {teams[game.team2_id]?.name}</TableCell>
-                            </TableRow>
-                        )
-                    })}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <div>
+            <div className='matchWeekSelector'>
+                <button onClick={() => setCurrentMatchweek(currentMatchweek - 1)} disabled={currentMatchweek === 1}>
+                Anterior Jornada
+                </button>
+                <h2>Jornada {currentMatchweek}</h2>
+                <button onClick={() => setCurrentMatchweek(currentMatchweek + 1)} disabled={currentMatchweek === 2}>
+                Siguiente Jornada
+                </button>
+            </div>
+            <TableContainer component={Paper}>
+                <Table aria-label='games table'>
+                    <TableHead>
+                        <TableRow>
+                            {/* <TableCell>Jornada</TableCell> */}
+                            <TableCell align='right'>Fecha</TableCell>
+                            <TableCell align='right'>Resultado</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    {/* <TableBody>
+                        {games && Object.keys(games).length > 0 && Object.keys(games).map(key=>{
+                            const game = games[key];
+                            return(
+                                <TableRow 
+                                key={game.id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 }, '&:hover': { backgroundColor: '#f2f2f2' } }}
+                                onClick={()=>{navigate(`/games/${game.id}`)}}
+                                >
+                                    <TableCell>{game.matchweek_id}</TableCell>
+                                    <TableCell align='right'>{game.date.split('T')[0]}</TableCell>
+                                    <TableCell align='right'>{teams[game.team1_id]?.name} {game.team1_score} - {game.team2_score} {teams[game.team2_id]?.name}</TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody> */}
+                    <TableBody>
+                    {filteredGames.map(game => (
+                        <TableRow
+                        key={game.id}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 }, '&:hover': { backgroundColor: '#f2f2f2' } }}
+                        onClick={() => navigate(`/games/${game.id}`)}
+                        >
+                        <TableCell>{game.date.split('T')[0]}</TableCell>
+                        <TableCell align='right'>{teams[game.team1_id]?.name} {game.team1_score} - {game.team2_score} {teams[game.team2_id]?.name}</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
     );
 }
