89d790c4dc230c998eee77158d472bfe9723127b
Agustin Moras <a.morascastro@gmail.com>
2023-12-09 11:08:04 -0300

    mw 13

--- server/client/src/components/GamesList/GamesList.js
+++ server/client/src/components/GamesList/GamesList.js
@@ -19,7 +19,7 @@ export default function GamesList(){
     const teams = useSelector(state => state.teams);
     const navigate = useNavigate();
 
-    const matchweek_number = 12;
+    const matchweek_number = 13;
 
     const [currentMatchweek, setCurrentMatchweek] = React.useState(matchweek_number);
 
